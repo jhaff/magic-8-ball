8da36a13d1395149915762791119e550aee57831
jhaff <jacobhaff2@gmail.com>
2018-09-22 16:48:54 -0700

    User can tap to see the 8ball's answer

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -14,15 +14,16 @@ class ViewController: UIViewController {
     
     let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
 
+    
+    @IBOutlet weak var shakeButton: UIButton!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     func generateAnswer() {
         let maxIndex = UInt32(answers.count)
         let randomIndex = Int(arc4random_uniform(maxIndex))
         
         answerLabel.text = answers[randomIndex]
     }
-    
-    @IBOutlet weak var shakeButton: UIButton!
-    @IBOutlet weak var answerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +34,7 @@ class ViewController: UIViewController {
         
         print("Shake it like a polaroid picture!")
         
-        answerLabel.text = "button was tapped"
+        generateAnswer()
 
 
         
