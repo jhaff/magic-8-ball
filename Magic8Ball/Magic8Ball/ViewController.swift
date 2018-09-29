b66a910a6d887e2a40a61b7bb1ab61fc68567e0c
jhaff <jacobhaff2@gmail.com>
2018-09-29 16:54:35 -0700

    Added comments and formatted code for clarity and readability

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
 
     // MARK: - Properties
     
-    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"] //array to store answers spat out by 8 ball
 
-    
+    //link UI attributes
     @IBOutlet weak var shakeButton: UIButton!
     @IBOutlet weak var answerLabel: UILabel!
     
+    //select a random answer from array and update the label
     func generateAnswer() {
         let maxIndex = UInt32(answers.count)
         let randomIndex = Int(arc4random_uniform(maxIndex))
@@ -27,19 +28,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
     }
     
+    //listen for taps on shakeButton, respond with answer
     @IBAction func shakeButtonTapped(_ sender: Any) {
         
         print("Shake it like a polaroid picture!")
         
         generateAnswer()
-
-
         
     }
 
+    //listen for shakes, respond with answer
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         guard motion == .motionShake else { return }
         
@@ -47,5 +48,3 @@ class ViewController: UIViewController {
     }
     
 }
-
-
